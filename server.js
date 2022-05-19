755b4c76768ea51e5b707f2368a256a27eca61fc
Katharine Swilley <kswilley@gmail.com>
2022-05-18 19:31:03 -0700

    initial commit for lab 8

--- server.js
+++ server.js
@@ -3,11 +3,9 @@
 console.log('our first server');
 // REQUIRE
 const express = require('express');
-
 require('dotenv').config();
-
-let data = require('./data/weather.json');
 const cors = require('cors');
+const axios = require('axios');
 
 // USE
 const app = express();
@@ -19,20 +17,29 @@ app.use(cors());
 // ROUTES
 app.get('/weather', (request, response, next) => {
   try {
-    let cityData = request.query.city;
+    // let cityData = request.query.city;
 
-    let selectedCity = data.find(city => city.city_name.toLowerCase() === cityData.toLowerCase());
+    // let selectedCity = data.find(city => city.city_name.toLowerCase() === cityData.toLowerCase());
 
-    let dataToSend = selectedCity.data.map(city => new Forecast(city));
-    response.send(dataToSend);
-    console.log(dataToSend);
+    // let dataToSend = selectedCity.data.map(city => new Forecast(city));
+    // response.send(dataToSend);
+    // console.log(dataToSend);
+
+    let search = request.query.searchQuery;
+
+    let url = `: https://api.weatherbit.io/v2.0/forecast/daily?city=${search}&units=I&days=7&key=${process.env.WEATHER_API_KEY}`;
+
+    let results = await axios.get(url);
+
+    //let latLon = data.find
+
   } catch (error) {
     next(error);
   }
 });
 
 app.get('*', (request, response) => {
-  response.send('does not exist');
+  response.status(404).send('Not found');
 });
 
 // ERRORS
@@ -44,8 +51,10 @@ app.use((error, request, response, next) => {
 // CLASSES
 class Forecast {
   constructor(weatherObject) {
-    this.date = weatherObject.datetime;
-    this.description = weatherObject.weather.description;
+    // this.date = weatherObject.datetime;
+    // this.description = weatherObject.weather.description;
+    this.lat=weatherObject.lat;
+    this.lon=weatherObject.lon;
   }
 }
 
